9727d0131771a3e38eaf04b99d86f24e20fccb39
radzserg <radzserg@gmail.com>
2015-04-08 10:49:06 +0300

    package name collision

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-    name: 'pagination',
+    name: 'radzserg:pagination',
     version: '0.0.1',
     // Brief, one-line summary of the package.
     summary: 'Meteor pagination package based on subscriptions',
