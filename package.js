c5a5e5db3ba2c86719c2111653cfa60b1ee5c486
radzserg <radzserg@gmail.com>
2015-04-08 10:47:13 +0300

    added ref to git

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
     name: 'pagination',
     version: '0.0.1',
     // Brief, one-line summary of the package.
-    summary: '',
+    summary: 'Meteor pagination package based on subscriptions',
     // URL to the Git repository containing the source code for this package.
-    git: '',
+    git: 'https://github.com/radzserg/meteor_pagination.git',
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
